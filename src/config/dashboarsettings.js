4b6fba5bb7114ee226eaf6f55064538ec7da6528
HellpMe <bruno.bh969@gmail.com>
2021-05-18 01:00:12 -0300

    ab

--- src/config/dashboarsettings.js
+++ src/config/dashboarsettings.js
@@ -2,11 +2,12 @@ module.exports = {
     //Itens para dashboard
         dashboord: 'true',
         domain: 'dashboardtst.herokuapp.com',
+        https:'https://',
         port: '80',
         clientID: '768000530361876490',
         clientSecret: 'RtWuEiVQu6obpNzD-xWjAO1eWbJcKaN6',
         mongoUrl: 'mongodb+srv://PrCoJs:PrCo.Js@project-columbine.4tkrg.mongodb.net/Dashb?retryWrites=true&w=majority',
-        verification: 'discordbot dashboard',
+        verification: '',
         description: 'discordbot',
         google_analitics: '',
         arc: '',
