5af55e5de19d7c7253781f1c767eb80ff99b82cc
HellpMe <bruno.bh969@gmail.com>
2021-05-18 01:05:47 -0300

    q merdahein

---
+++ src/config/config.js
@@ -0,0 +1,32 @@
+const config = {
+    ownerID: '583676051838861342', //Owner ID//Seu ID
+    token: 'ODM4MjAyNzk4NjMzNTE3MTI3.YI3rMA.VPbpxdv093pc92HRkDK9DMSvm_U', //https://discord.com/developers/applications
+    //Pegando as keys das API'S //GET_API_KEYS 
+    api_keys: { //https://developer.spotify.com/dashboard/applications
+        spotify: {
+            iD: '5e76bb1f956c472b8fbef8108c04a222',
+            secret: '74610f3484c5464589f8dbb6d9d3b27d'
+        },
+        genius: 'b7jTFhEmuIYuqsPFer_yzvuEU2zJmHrc0PhoXb4r45APGJJdhx8dWnIKwakH4F8e', //https://genius.com/api-clients
+    },
+    DiscordBotLists: {
+        DBL_key: '',
+    },
+    //Support server ID/Channel 
+    SupportServer: {
+        serverID: '718853480097382430',
+        serverChannel: '718853480097382435'
+    },
+    defaultSettings: {
+        prefix: '!', //YouPrefix here
+        Language: 'pt-Br',
+        Dashboard: true,
+    },
+    //Mostrando o ping continuo do uptime do bot
+    PING_PORT: 65535,
+    //MongoDB URL
+    MongoDBURl: 'mongodb+srv://PrCoJs:PrCo.Js@project-columbine.4tkrg.mongodb.net/PrCo?retryWrites=true&w=majority',
+    //Para ajudar na correção de bugs!
+    debug: false,
+}
+module.exports = config;
---
+++ src/config/webhooks.js
@@ -0,0 +1,12 @@
+const webhooks = {
+    //webhooks dos status shards
+    Webhook_ID: "838524366923759626", //webhook id
+    Webhook_Channel_ID: "838519993821298778", //webhook channel_id
+    Webhook_Token: "P2yWzEb6G3fqdAaCgLtQZpUzu9AhAm4z65RyTITmylyKrAQPP7lkHTPOfA_zM6AAqqel", //webhook token
+    
+    //webhooks dos erro dos shards
+    Error_Webhook_ID: "838524553868083281", //webhook id
+    Error_Webhook_Channel_ID: "838521451367104582",
+    Error_Webhook_Token: "c2sxPBJtQARRv5LCtIMAqQhyZnH28dlgBhSBcTUG88J6k4P4-by9-gchgADion-bxOTS", //webhook token
+};
+module.exports = webhooks;
