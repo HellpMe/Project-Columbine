d45a7b536283eeaaec477aa544f59b3321c9952c
HellpMe <bruno.bh969@gmail.com>
2021-05-19 23:00:54 -0300

    ab

--- src/config/dashboarsettings.js
+++ src/config/dashboarsettings.js
@@ -1,9 +1,9 @@
 module.exports = {
     //Itens para dashboard
         dashboord: 'true',
-        domain: 'https://testedashb.herokuapp.com',
+        domain: 'testedashb.herokuapp.com',
         https:'https://',
-        port: '5003',
+        port: '',
         clientID: '838202798633517127',
         clientSecret: 'sdh1tqGXM-a9kD9jjLNwnchhFEqe8Klx',
         mongoUrl: 'mongodb+srv://PrCoJs:PrCo.Js@project-columbine.4tkrg.mongodb.net/Dashb?retryWrites=true&w=majority',
